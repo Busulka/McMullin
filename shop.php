1cbe59096c77f288c27964512155bb4c91337ace
Busulka <83404095+Busulka@users.noreply.github.com>
2022-06-07 18:03:02 +0300

    shop css

--- shop.php
+++ shop.php
@@ -9,7 +9,10 @@
     <link rel="stylesheet" href="styles/header.css">
     <link rel="stylesheet" href="styles/menu.css">
     <link rel="stylesheet" href="styles/footer.css">
+    <link rel="stylesheet" href="styles/shop.css">
+
     <script src="https://kit.fontawesome.com/2436301015.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="//www.auctionnudge.com/feed/item/js/theme/unstyled/page/init/img_size/360/cats_output/dropdown/search_box/1/lang/english/SellerID/mcmullinplymouth/siteid/3/MaxEntries/6"></script>
 
 
 </head>
@@ -19,18 +22,20 @@ include "templates/header.php";
 include "templates/menu.php";
 ?>
 
-    <script type="text/javascript" src="//www.auctionnudge.com/feed/item/js/theme/grid/page/init/img_size/200/cats_output/dropdown/search_box/1/blank/1/grid_cols/2/grid_width/80%25/lang/english/SellerID/mcmullinplymouth/siteid/3/MaxEntries/6"></script>
 
-    <h1>Shop</h1>
 
     <div class="main container">
+
         <div class="main__left">
+            <h1>Shop</h1>
             <div id="auction-nudge-items" class="auction-nudge"></div>
         </div>
         <?php
         include "templates/sidebar.php"
         ?>
     </div>
+
+
 
 <?php
 include "templates/footer.php";
